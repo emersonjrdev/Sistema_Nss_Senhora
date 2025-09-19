07933577c8223405bbfbafa8ddaaf9222ddae8d9
EmersonJr <152315731+emersonjrdev@users.noreply.github.com>
2025-09-19 02:34:18 -0300

    agora vai

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,14 +2,19 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },            // nome do usuário
-    email: { type: String, unique: true },             // email único (se for usar)
-    photo: { type: String },                           // URL da imagem no Cloudinary
+    name: { type: String, required: true }, // nome do usuário
+    email: {
+      type: String,
+      unique: false, // remove a restrição
+      sparse: true, // só aplica unique se existir valor
+    },
+    // email único (se for usar)
+    photo: { type: String }, // URL da imagem no Cloudinary
 
     // campos extras do formulário
     funcao: { type: String },
     inicio: { type: Date },
-    local: { type: String }
+    local: { type: String },
   },
   { timestamps: true }
 );
